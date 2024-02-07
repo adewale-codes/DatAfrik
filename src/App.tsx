d440ed5057e8c4ebdb4dc541dfa67f8b058249c5
adewale-codes <adewaleabiola78@gmail.com>
2024-02-07 10:15:54 +0100

    created supported section and made it responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Supported from './components/Supported'
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
       <Nav />
       <Hero />
+      <Supported />
     </>
   )
 }
---
+++ src/components/Supported.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Supported = () => {
+  return (
+    <div className='px-5 md:px-24 pt-8 md:pt-52 md:pb-20'>
+        <div className='flex items-center justify-center'>
+            <div className='font-bold text-white'>Supported and backed by:</div>
+        </div>
+        <div className='flex pt-2 md:pt-9 items-center justify-center'>
+            <div className='flex gap-4 md:flex-row flex-col items-center md:items-start pt-5'>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Lorem</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Ipsum</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Dolor</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Sit</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='flex items-center justify-center'>
+            <div className='flex gap-4 md:flex-row flex-col items-center md:items-start pt-5'>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Dolor</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Sit</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Lorem</div>
+                    </div>
+                </div>
+                <div className=''>
+                    <div className='bg-grays-100 w-56 h-16 flex justify-center items-center'>
+                        <div className=' text-center font-bold text-3xl text-black'>Ipsum</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='flex items-center justify-center pt-5 md:pt-9'>
+            <div>
+                <div className='font-bold text-xl md:text-3xl text-white text-center'>Less code. More product.</div>
+                <div className='flex items-center justify-center'>
+                    <div className='text-white text-sm md:text-base text-center md:max-w-[70%] max-w-[90%]'>By using our UI Kit, we make sure you only get to <span className='text-primary-700 font-bold'>focus more</span> on the other things that matter.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Supported
